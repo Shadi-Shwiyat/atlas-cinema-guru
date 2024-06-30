bcde82ea1238a34f6124057fbb1288a912d688b6
Shadi-Shwiyat <raz611103@gmail.com>
2024-06-30 16:00:11 -0500

    updated app.js, will continue finishing useEffect hook tomorrow

--- src/App.js
+++ src/App.js
@@ -1,66 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
-import { faUser } from '@fortawesome/free-solid-svg-icons';
-import { faKey } from '@fortawesome/free-solid-svg-icons'
-import Input from './components/general/Input.js';
-import SelectInput from './components/general/SelectInput.js';
-import Button from './components/general/Button.js';
-import SearchBar from './components/general/SearchBar.js';
 
 function App() {
-  const [value, setValue] = useState('');
-  const [numValue, setNumValue] = useState(1970);
-  const [selectValue, setSelectValue] = useState('default');
-  const [title, setTitle] = useState('');
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userUsername, setUserUsername] = useState('');
 
-  const onClick = (e) => {
-    e.preventDefault();
-    console.log('Button has been clicked!');
-  }
+  useEffect(() => {
+    // Get the access token from local storage only when
+    // the component mounts
+    const accessToken = localStorage.getItem('accessToken');
+
+    // Send post request to /api/auth/ with auth header 
+    // with accessToken
+    
+
+  }, [])
+
   return (
     <div className="App">
-      <Input 
-      label='Username:'
-      type='text'
-      className='text-input'
-      value={value}
-      setValue={setValue}
-      icon={faUser}
-       />
-      <p>{'\n\n'}</p>
-      <Input 
-      label='Min Date:'
-      type='number'
-      className='date-input'
-      value={numValue}
-      setValue={setNumValue}
-      inputAttributes={{min: '1970', max: '2024'}}
-       />
-      <p>{'\n\n'}</p>
-      <SelectInput
-      label='Sort:'
-      options={[{label: 'default', value: 'default'}, {label: 'latest', value: 'latest'}]}
-      className={'select-input'}
-      value={selectValue}
-      setValue={setSelectValue}
-       />
-      <p>{'\n\n'}</p>
-      <Button
-      label='Load More...'
-      className='button'
-      onClick={onClick}
-       />
-      <p>{'\n\n'}</p>
-      <Button
-      label='Sign In'
-      className='sign-on-button'
-      onClick={onClick}
-      icon={faKey}
-       />
-      <p>{'\n\n'}</p>
-      <SearchBar
-      title={title}
-      setTitle={setTitle} />
+
     </div>
   );
 }
