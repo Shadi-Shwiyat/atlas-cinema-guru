0dfedc5f9cb3c6540e084d77082a051ff827b8fc
Shadi-Shwiyat <raz611103@gmail.com>
2024-07-01 11:47:51 -0500

    task 3 creating main app component that checks user auth on mount, renders corresponding component

--- src/App.js
+++ src/App.js
@@ -9,16 +9,38 @@ function App() {
     // Get the access token from local storage only when
     // the component mounts
     const accessToken = localStorage.getItem('accessToken');
+    console.log('Access Token is:', accessToken);
 
-    // Send post request to /api/auth/ with auth header 
-    // with accessToken
-    
+    // Send post request to /api/auth/ with auth header
+    // set to Bearer <accessToken>
+    const authRequest = async () => {
+      try {
+        const response = await fetch('http://localhost:8000/api/auth', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${accessToken}`,
+            'Accept': 'application/json',
+          }
+        })
 
+        if (response.status == 200) {
+          const responseData = await response.json();
+          const userName = responseData['username'];
+          setUserUsername(userName);
+          setIsLoggedIn(true);
+        }
+      } catch (error) {
+        console.error('Error with api/auth/ route:', error);
+      }
+    }
+
+    authRequest();
   }, [])
 
   return (
     <div className="App">
-
+      {/* {isLoggedIn ? <Dashboard /> : <Authentication />} */}
     </div>
   );
 }
